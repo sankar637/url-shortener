eabb2472775c735bfc1a107113e61f7655cf905e
sankar637 <sankarperugu@gmail.com>
2026-08-21 14:35:32 +0530

    Update frontend API for production

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://url-shortener-api-.onrender.com";
 
 function App() {
   const [url, setUrl] = useState("");
